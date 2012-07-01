8497c64cc5a3ec1837cc285593023c9fe323af15
grdn <grdn@ce915c6f-743e-4cf8-b243-f6f642e64709>
2012-07-01 22:42:15 +0000

    Edited TOC, added pkgmeta!

---
+++ Locales/enUS.lua
@@ -0,0 +1,10 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("iMoney", "enUS", true)
+
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["Today Session"] = "Today Session"
+L["Total Gold"] = "Total Gold"
+L["Gains"] = "Gains"
+L["Losses"] = "Losses"
+--@end-do-not-package@
