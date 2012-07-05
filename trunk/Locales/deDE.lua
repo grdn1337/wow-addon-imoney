919750e066fc5b72cedbc997742742243604563f
grdn <grdn@ce915c6f-743e-4cf8-b243-f6f642e64709>
2012-07-05 11:11:50 +0000

    - Added localization support for ALL locales. They just have to be updated via Curseforge now!

--- trunk/Locales/deDE.lua
+++ trunk/Locales/deDE.lua
@@ -1,11 +1,15 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("iMoney", "deDE")
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "deDE")
+
 if not L then return end
 
 --@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
 
 --@do-not-package@
-L["Today Session"] = "Heutige Sitzung"
-L["Total Gold"] = "Gold Gesamt"
+L["Confirm to delete from iMoney!"] = "Best\195\164tigen, um aus iMoney zu entfernen!"
 L["Gains"] = "Einnahmen"
 L["Losses"] = "Ausgaben"
+L["Right-click to remove"] = "Rechtsklick zum Entfernen"
+L["Today Session"] = "Heutige Sitzung"
+L["Total Gold"] = "Gold Gesamt"
 --@end-do-not-package@
