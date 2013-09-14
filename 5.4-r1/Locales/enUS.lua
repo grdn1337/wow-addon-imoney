66b34f1559350b236b1dee2d25d38b50fd064796
grdn <grdn@ce915c6f-743e-4cf8-b243-f6f642e64709>
2013-09-14 07:48:41 +0000

    See r23

---
+++ 5.4-r1/Locales/enUS.lua
@@ -0,0 +1,13 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
+
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["Today Session"] = true
+L["Total Gold"] = true
+L["Gains"] = true
+L["Losses"] = true
+L["Confirm to delete from iMoney!"] = true
+L["Right-click to remove"] = true
+--@end-do-not-package@
