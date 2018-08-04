3757ad5f75b9089146b56f6970cd0bb2da0c3124
grdn <grdn@ce915c6f-743e-4cf8-b243-f6f642e64709>
2018-08-04 21:17:09 +0000

    - Added new localization strings

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -10,4 +10,12 @@ L["Gains"] = true
 L["Losses"] = true
 L["Confirm to delete from iMoney!"] = true
 L["Right-click to remove"] = true
+
+L["Profit"] = true
+L["Today"] = true
+L["Week"] = true
+L["Month"] = true
+L["Overall"] = true
+L["Other Realms"] = true
+L["Characters"] = true
 --@end-do-not-package@
